744e41cf2dea855772b1c6928023686ba10cd587
jrodd86 <132295384+jrodd86@users.noreply.github.com>
2023-07-05 23:22:26 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(()=> {
+  $('#magicButton').click(()=>{
+    $('#card').fadeOut(); 
+    });
+  $('#magicButton2').click(()=>{
+    $('#casino').fadeOut();
+  });
+  $('#magicButton3').click(()=>{
+    $('#stars').fadeOut(); 
+  });
+    $('#magicButton4').click(()=>{
+    $('#wb_sunny').fadeOut();  
+  });
+    $("button").click(function(){
+    $("#p1").css("color", "red").slideUp(2000).slideDown(2000);
+  });
+  });
+
+
